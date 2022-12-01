a3085bb4defcb0b73db5131ad2280087d592dabd
maggie.li <maggie.yumeng.li@gmail.com>
2022-11-30 22:20:42 -0800

    home page

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,12 +5,6 @@ export default function Topbar() {
   const user = true;
   return (
     <div className="top">
-      <div className="topLeft">
-        <i className="topIcon fab fa-facebook-square"></i>
-        <i className="topIcon fab fa-instagram-square"></i>
-        <i className="topIcon fab fa-pinterest-square"></i>
-        <i className="topIcon fab fa-twitter-square"></i>
-      </div>
       <div className="topCenter">
         <ul className="topList">
           <li className="topListItem">
