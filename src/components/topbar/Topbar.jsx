5da6c60f18bd25c4844322bdec5d21d6fb0cce3b
maggie.li <maggie.yumeng.li@gmail.com>
2022-12-03 02:02:37 -0800

    commit Home page

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,18 +2,15 @@ import { Link } from "react-router-dom";
 import "./topbar.css";
 
 export default function Topbar() {
-  const user = true;
+  const user = false;
   return (
     <div className="top">
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">
-            <Link className="link" to="/">HOME</Link>
-          </li>
+          <li className="topListItem"><Link className="link" to="/">HOME</Link></li>
           <li className="topListItem">ABOUT</li>
-          <li className="topListItem">
-            <Link className="link" to="/write">WRITE</Link>
-          </li>
+          <li className="topListItem"><Link className="link" to="/write">WRITE</Link></li>
+
           {user ? (
               <Link className="link" to="/profile">
                 <li className="topListItem">PROFILE</li>
@@ -24,15 +21,13 @@ export default function Topbar() {
                   <Link className="link" to="/login">LOGIN</Link>
                 </li>
                 <li className="topListItem">
-                  <Link className="link" to="/register">
-                    REGISTER
-                  </Link>
+                  <Link className="link" to="/register">REGISTER</Link>
                 </li>
               </ul>
           )}
+
           {user && <li className="topListItem">LOGOUT</li>}
           <i className="topSearchIcon fas fa-search"></i>
-
         </ul>
       </div>
     </div>
