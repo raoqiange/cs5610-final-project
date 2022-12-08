852a91a61bdcce1c78d6355202d854b9422a78d0
A3M4 <419714952@qq.com>
2022-12-08 06:42:24 -0500

    update home page

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,44 +1,52 @@
-import { Link } from "react-router-dom";
-import "./sidebar.css";
+import { Link } from 'react-router-dom';
+import './sidebar.css';
 import React from 'react';
 
-export default function Sidebar() {
-  return (
-    <div className="sidebar">
-      <div className="sidebarItem">
-        <span className="sidebarTitle">About AniAni</span>
-        <img
-          src="https://img.freepik.com/premium-photo/woman-portrait-park-anime-manga-style_691560-1170.jpg"
-          alt=""
-        />
-        <p>
-          Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
-          amet ex esse.Sunt eu ut nostrud id quis proident.
-        </p>
-      </div>
-      <div className="sidebarItem">
-        <span className="sidebarTitle">CATEGORIES</span>
-        <ul className="sidebarList">
-          <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Life">Action</Link>
-          </li>
-          <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Music">Comedy</Link>
-          </li>
-          <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Sport">Drama</Link>
-          </li>
-          <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Style">Tragedy</Link>
-          </li>
-          <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Tech">History</Link>
-          </li>
-          <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Cinema">Magic</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+export default function Sidebar(props) {
+    const navHandler = (type) => {
+        props.filterPost && props.filterPost(type);
+    };
+    return (
+        <div className='sidebar'>
+            <div className='sidebarItem'>
+                <span className='sidebarTitle'>About AniAni</span>
+                <img src='https://img.freepik.com/premium-photo/woman-portrait-park-anime-manga-style_691560-1170.jpg' alt='' />
+                <p className='sidebarPara'>
+                    AniAni is the ultimate destination for anime fans! With a vast collection of popular and classic anime
+                    titles, you'll find hours of entertainment on our website. Our user-friendly interface makes it easy
+                    to search for your favorite shows and discover new ones. Join the AniAni community today and get ready to be
+                    transported to the world of anime!
+                </p>
+            </div>
+            <div className='sidebarItem'>
+                <span className='sidebarTitle'>CLICK TO FILTER!</span>
+                <ul className='sidebarList'>
+                    <li className='sidebarListItem' onClick={() => navHandler('Action')}>
+                        Action
+                    </li>
+                    <li className='sidebarListItem' onClick={() => navHandler('Comedy')}>
+                        Comedy
+                    </li>
+                    <li className='sidebarListItem' onClick={() => navHandler('Drama')}>
+                        Drama
+                    </li>
+                    <li className='sidebarListItem' onClick={() => navHandler('Sci-Fi')}>
+                        Sci-Fi
+                    </li>
+                    <li className='sidebarListItem' onClick={() => navHandler('Fantasy')}>
+                        Fantasy
+                    </li>
+                    <li className='sidebarListItem' onClick={() => navHandler('Adventure')}>
+                        Adventure
+                    </li>
+                    <li className='sidebarListItem' onClick={() => navHandler('Suspense')}>
+                        Suspense
+                    </li>
+                    <li className='sidebarListItem' onClick={() => navHandler('Mystery')}>
+                        Mystery
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,42 +1,60 @@
-import { useLocation } from "react-router";
-import {useDispatch, useSelector} from "react-redux";
-import {getAllAnimeThunk, getAnimeDetailThunk, searchAnimeThunk, getRecentlyReviewedAnimeByUsernameThunk} 
-    from "../../services/anime/anime-thunks";
-import Header from "../../components/header/Header";
-import Posts from "../../components/posts/Posts";
-import Sidebar from "../../components/sidebar/Sidebar";
-import React, {useEffect} from 'react';
-import "./homepage.css";
-import Post from "../../components/post/Post";
+import { useLocation } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    getAllAnimeThunk,
+    getAnimeDetailThunk,
+    searchAnimeThunk,
+    getRecentlyReviewedAnimeByUsernameThunk,
+} from '../../services/anime/anime-thunks';
+import Header from '../../components/header/Header';
+import Posts from '../../components/posts/Posts';
+import Sidebar from '../../components/sidebar/Sidebar';
+import React, { useEffect, useState } from 'react';
+import './homepage.css';
+import Post from '../../components/post/Post';
 
 export default function Homepage() {
-
     const dispatch = useDispatch();
-    const {
-        allAnimeList,
-        loadingAllAnime
-    } = useSelector(state => state.anime);
+    const { allAnimeList, loadingAllAnime } = useSelector((state) => state.anime);
+    const [postData, setPostData] = useState(allAnimeList);
 
     useEffect(() => {
         dispatch(getAllAnimeThunk());
-    }, [dispatch])
+    }, [dispatch]);
 
     console.log(allAnimeList);
+    useEffect(() => {
+        setPostData(allAnimeList);
+    }, [allAnimeList]);
 
+    const filterPost = (type) => {
+        const newData = allAnimeList.filter((item) => item.genres.includes(type));
+        setPostData(newData);
+    };
     // anonymous user & admin => allAnime
     // fan & forum owner => recently reviewed anime
     return (
-      <>
-        <Header />
-        <div className="home">
-            <div className="anime">
-            {!loadingAllAnime && allAnimeList.map(anime =>
-                <div key={anime._id}> <Post img={anime.image} id={anime._id}
-                                                              title={anime.title} synopsis={anime.synopsis}
-                                                              status={anime.status} genres={anime.genres}/> </div>)}
+        <>
+            <Header />
+            <div className='home'>
+                <div className='anime'>
+                    {!loadingAllAnime &&
+                        postData.map((anime) => (
+                            <div key={anime._id}>
+                                {' '}
+                                <Post
+                                    img={anime.image}
+                                    id={anime._id}
+                                    title={anime.title}
+                                    synopsis={anime.synopsis}
+                                    status={anime.status}
+                                    genres={anime.genres}
+                                />{' '}
+                            </div>
+                        ))}
+                </div>
+                <Sidebar filterPost={(type) => filterPost(type)} />
             </div>
-          <Sidebar />
-        </div>
-      </>
+        </>
     );
-  }
+}
