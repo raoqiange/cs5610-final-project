153c9d6506046b899c64487dfcc2b6a42b779a21
Rachel Rao <rao.q@northeastern.edu>
2022-12-09 14:29:41 -0800

    update home page with recently reviewed anime

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,9 +1,6 @@
-import { useLocation } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import {
     getAllAnimeThunk,
-    getAnimeDetailThunk,
-    searchAnimeThunk,
     getRecentlyReviewedAnimeByUsernameThunk,
 } from '../../services/anime/anime-thunks';
 import Header from '../../components/header/Header';
@@ -15,7 +12,8 @@ import Post from '../../components/post/Post';
 
 export default function Homepage() {
     const dispatch = useDispatch();
-    const { allAnimeList, loadingAllAnime } = useSelector((state) => state.anime);
+    const { allAnimeList, loadingAllAnime, recentlyReviewedAnimeList, loadingRecentlyReviewedAnime}
+        = useSelector((state) => state.anime);
     const [postData, setPostData] = useState(allAnimeList);
     const {currentUser} = useSelector(state => state.users)
     console.log(currentUser)
@@ -24,10 +22,16 @@ export default function Homepage() {
         dispatch(getAllAnimeThunk());
     }, [dispatch]);
 
-    console.log(allAnimeList);
+    // console.log(allAnimeList);
     useEffect(() => {
         setPostData(allAnimeList);
     }, [allAnimeList]);
+
+    useEffect(()=> {
+        currentUser && dispatch(getRecentlyReviewedAnimeByUsernameThunk(currentUser.username))
+    }, [currentUser])
+
+    console.log(recentlyReviewedAnimeList)
 
     const filterPost = (type) => {
         const newData = allAnimeList.filter((item) => item.genres.includes(type));
@@ -39,7 +43,35 @@ export default function Homepage() {
     return (
         <>
             <Header />
+            {currentUser && currentUser.role !== "ADMIN" &&
+                <>
+                    <h2 className='text-title'>You've recently reviewed these anime...</h2>
+                    <div className='anime'>
+                        {!loadingRecentlyReviewedAnime && recentlyReviewedAnimeList.length !== 0 &&
+                            recentlyReviewedAnimeList.map((anime) => (
+                                <div key={anime.anime_id}>
+                                    {' '}
+                                    <Post
+                                        img={anime.image}
+                                        id={anime.anime_id}
+                                        title={anime.title}
+                                        synopsis={anime.synopsis}
+                                    />{' '}
+                                </div>
+                            ))}
+                        {!loadingRecentlyReviewedAnime && recentlyReviewedAnimeList.length === 0 &&
+                            <>
+                                <h2 className='no-history-text'>Looks like you don't have review history...</h2>
+                                <img className="charging-img" src="https://media.tenor.com/zoWAtwWVq4AAAAAC/miku-anime.gif"/>
+                            </>
+
+                        }
+                    </div>
+                    <h2 className='text-title'>Start Exploring All Anime....</h2>
+                </>
+            }
             <div className='home'>
+                {/*display for all users*/}
                 <div className='anime'>
                     {!loadingAllAnime &&
                         postData.map((anime) => (
