580deeb5534a923cafaec7487d77be7d70d698b2
maggie.li <maggie.yumeng.li@gmail.com>
2022-12-09 20:27:21 -0800

    commit update forum style

--- src/pages/forum/Forum.jsx
+++ src/pages/forum/Forum.jsx
@@ -41,35 +41,37 @@ export default function Forum() {
                 className="writeImg"
                 src="https://assets.reedpopcdn.com/Genshin-Impact-anime.jpg/BROK/thumbnail/1600x900/quality/100/Genshin-Impact-anime.jpg"
                 alt=""/>
+
+            <div className="writeFormGroup"  >
+            <div className="managesWrapper">
             {/*add style for post here please*/}
             {!postsLoading && allPosts.map(p =>
                 <div>
-                    <p>
+                    <p className="forumFontSize">
                         <Link to={'/public/' + p.author_username}>
                             {p.author_username}</Link>
                         : {p.title}</p>
                     <Link to={'/post_comments/' + p._id}>
-                        <p> {p.description} </p>
+                        <p className="forumFontStyle"> {p.description} </p>
                     </Link>
                     {currentUser &&
                     (currentUser.role === 'ADMIN' || (currentUser.role === 'FORUM_AUTHOR' && currentUser.username === p.author_username)) &&
-                    <button onClick={()=>dispatch(deleteForumPostByIdThunk(p._id))}>Delete</button>
+                    <button className="forumsDeleteButton" onClick={()=>dispatch(deleteForumPostByIdThunk(p._id))}>Delete</button>
                     }
                     <br/>
+
                 </div>
             )}
+            </div>
+            </div>
             {currentUser && currentUser.role === 'FORUM_AUTHOR' &&
             <div className="write">
                 <form className="writeForm">
-                    <div className="writeFormGroup">
-                        <label htmlFor="fileInput">
-                            <i className="writeIcon fas fa-plus"></i>
-                        </label>
-                        <input id="fileInput" type="file" style={{display: "none"}}/>
+                    <div className="writeFormGroup" className="forumMargin">
                         <input
                             id="new-post-title"
                             className="writeInput"
-                            placeholder="Title"
+                            placeholder="Please Enetr Your Title"
                             type="text"
                             autoFocus={true}/>
                     </div>
@@ -83,5 +85,6 @@ export default function Forum() {
             </div>
             }
         </div>
+
     );
 }
