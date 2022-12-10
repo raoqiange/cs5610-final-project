f0a40457671f81e0662a594001e29b5c94f88ae9
A3M4 <419714952@qq.com>
2022-12-10 00:06:17 -0500

    Add shrink effect to banner img

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,46 @@
 import "./header.css";
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Header() {
-    return (
-        <div className="header">
-            <div className="headerTitles">
-                <span className="headerTitleSm">Anime Planet</span>
-                <span className="headerTitleLg">AniAni</span>
+
+export default class Header extends Component {
+    state = {
+        isOpen: false,
+    }
+    handleToggle = () => {
+        this.setState({isOpen: !this.state.isOpen});
+    }
+    componentDidMount() {
+        window.addEventListener("scroll", this.resizeHeaderOnScroll);
+    }
+    resizeHeaderOnScroll() {
+        const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 50,
+            headerEl = document.getElementById("banner");
+        if (distanceY > shrinkOn) {
+            if (headerEl !== null) {
+                headerEl.classList.add("bannerShrink");
+            }
+
+        } else {
+            if (headerEl !== null) {
+                headerEl.classList.remove("bannerShrink");
+            }
+        }
+    }
+    render() {
+        return (
+            <div className="header">
+                <div className="headerTitles">
+                    <span className="headerTitleSm">Anime Planet</span>
+                    <span className="headerTitleLg">AniAni</span>
+                </div>
+                <img
+                    className="headerImg"
+                    id = "banner"
+                    src = "https://wallpapersmug.com/download/2048x1152/0f8bba/bunny-ears-anime-white-dress.jpg"
+                    alt=""
+                />
             </div>
-            <img
-                className="headerImg"
-                // src="http://img0.joyreactor.com/pics/comment/full/banner-art-anime-speed-grapher-886729.png"
-                src = "https://wallpapersmug.com/download/2048x1152/0f8bba/bunny-ears-anime-white-dress.jpg"
-                alt=""
-            />
-        </div>
-    );
+        );
+    }
 }
